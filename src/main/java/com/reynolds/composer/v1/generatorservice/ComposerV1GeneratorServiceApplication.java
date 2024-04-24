5807d79ca34c7d6c34cea894c6a45d708ea86060
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2024-04-24 15:34:50 +0100

    Initial module commit.

---
+++ src/main/java/com/reynolds/composer/v1/generatorservice/ComposerV1GeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reynolds.composer.v1.generatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposerV1GeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComposerV1GeneratorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reynolds/composer/v1/generatorservice/ComposerV1GeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynolds.composer.v1.generatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposerV1GeneratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
